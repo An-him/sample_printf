10756ccd9265f74dae2fb7999e0f1f6b40685908
kofimuad <iamkofiagyei@gmail.com>
2024-01-29 02:05:59 +0000

    pushed main.h and _printf

--- main.h
+++ main.h
@@ -1,4 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#endif
+/* A list of my header files to use */
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+/* Here are a list of my prototypes */
+
+int _printf(const char *format, ...);
+
+
+#endif /*ALX*/
