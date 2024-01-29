531a89bfe6a4978e148ec65acaa68c99fc7e657f
kofimuad <iamkofiagyei@gmail.com>
2024-01-29 03:22:43 +0000

    a few changes

--- _printf.c
+++ _printf.c
@@ -68,7 +68,7 @@ int _printf(const char *format, ...)
 int main(void)
 {
 	_printf("My name is Bismark\n");
-	_printf("My last name is: %s", "Agyei");
+	_printf("My last name is: %s\n", "Agyei");
 	_printf("I am of sex:  %c\n", 'M');
 	_printf("I would love to use the %% sign today\n");
 }
